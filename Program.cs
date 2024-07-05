dc0c1ae054cf6c61ecaee6fb624420dfb2b39f32
rLosito <romeo.losito@progettopa.it>
2024-07-05 13:07:15 +0200

    Commit in new-br

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sono nel branch new-br !!");
         }
     }
 }
